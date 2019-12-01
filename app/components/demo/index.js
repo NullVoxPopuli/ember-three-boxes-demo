a9bf106d1376e50f9093c37b35e9a20e3fafc0fa
NullVoxPopuli <LPSego3+dev@gmail.com>
2019-12-01 08:03:27 -0500

    re-do everything

---
+++ app/components/demo/index.js
@@ -0,0 +1,39 @@
+import Component from '@glimmer/component';
+import { tracked } from '@glimmer/tracking';
+import { action, computed } from '@ember/object';
+
+import {WebGlHelper} from './web-gl-helper';
+
+function random() {
+  return Math.random() * 360;
+}
+
+function newRotations(num = 20) {
+  return Array(num).fill().map(() => [random(), random(), random()]);
+}
+
+export default class DemoComponent extends Component {
+  @tracked count;
+
+  @tracked rotations = newRotations();
+  @tracked fps = 0;
+
+  @action
+  updateCount(newCount) {
+    this.count = newCount;
+    this.rotations = newRotations(this.count);
+
+    this.renderer.syncBoxes(this.rotations);
+  }
+
+  @action
+  setup(element) {
+    this.renderer = new WebGlHelper({
+      container: element,
+      onFPSUpdate: (fps) => this.fps = fps,
+    });
+
+    this.renderer.animate();
+  }
+}
+
